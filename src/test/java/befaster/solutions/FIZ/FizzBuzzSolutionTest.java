0d2f68e17e1c356e88222f543f04ce39452a0d8f
sbx_user1060 <sbx_user1060@ip-10-128-134-167.eu-west-2.compute.internal>
2018-12-07 22:46:20 +0000

    Fri Dec 07 22:46:20 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,5 +1,15 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FizzBuzzSolutionTest {
 
+
+    @Test
+    public void fizzBuzz() {
+        Assert.fail("Not implemented");
+    }
 }
